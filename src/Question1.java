3155658dde8d86e85c9280fead25dc6c821ffe80
nolife <voidky@Hotmail.com>
2022-02-08 22:03:38 +0800

    Added User input to q1

--- src/Question1.java
+++ src/Question1.java
@@ -2,13 +2,22 @@ import java.text.Collator;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class Question1 {
 
     public static void main(String[] args){
-        LinkedList<Integer> list = new LinkedList<>(Arrays.asList(1,3,5,7,9,11));
+        Scanner sc = new Scanner(System.in);
+        LinkedList<Integer> list = new LinkedList<>();
+        System.out.print("Enter a list in ascending order : ");
+        while (sc.hasNextInt()){
+            list.add(sc.nextInt());
+        }
         System.out.println("Before: " + list);
-        addAndSort(list, 6);
+        System.out.print("Enter a value to be added into the list : ");
+        sc.next();
+        int val = sc.nextInt();
+        addAndSort(list, val);
         System.out.println("After: " + list);
     }
 
