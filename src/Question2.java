79a01d7e63ac14b12070d45b2c1c1bbd4d53bc97
nolife <voidky@Hotmail.com>
2022-02-08 22:06:27 +0800

    Added user input to q2

--- src/Question2.java
+++ src/Question2.java
@@ -1,12 +1,17 @@
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class Question2 {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
         LinkedList<Integer> list = new LinkedList<>(Arrays.asList(1,3,5,7,9,11));
         System.out.println("Before: " + list);
-        swapValues(list, 1, 5);
+        System.out.println("Enter the index of the two value to be swapped : ");
+        int indexOne = sc.nextInt();
+        int indexTwo = sc.nextInt();
+        swapValues(list, indexOne, indexTwo);
         System.out.println("After: " + list);
     }
 
